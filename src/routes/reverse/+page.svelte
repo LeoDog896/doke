2b33f11b24dec058dc30191a5ef258552035cbac
Tristan F <LeoDog896@hotmail.com>
2022-12-01 18:50:40 +0000

    Reverse text

---
+++ src/routes/reverse/+page.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+	let input = '';
+	$: reverse = input.split("").reverse().join("")
+</script>
+
+<h1>Reverse Text</h1>
+
+<input bind:value={input} placeholder="Enter Input" />
+<p>{reverse}</p>
