0529d8f48b20f24f7872c1a524496fd76c3d82d5
LeoDog896 <LeoDog896@hotmail.com>
2022-12-01 20:21:10 +0000

    base64

---
+++ src/routes/base64/+page.svelte
@@ -0,0 +1,29 @@
+<script lang="ts">
+	let toEncrypt = ''
+
+	$: encryptedResult = window.btoa(toEncrypt)
+
+	let toDecrypt = ''
+
+	let decryptedResult = ''
+
+	$: {
+		try {
+			decryptedResult = window.atob(toDecrypt)
+		} catch {
+			decryptedResult = "Result could not be decrypted."
+		}
+	}
+</script>
+
+<h1>Base64 encryption</h1>
+
+<input bind:value={toEncrypt}>
+
+<p>{encryptedResult}</p>
+
+<h1>Base64 decryption</h1>
+
+<input bind:value={toDecrypt}>
+
+<p>{decryptedResult}</p>
