1f073b5c535242847b02f11ff6fdbfe7aa30d54d
LeoDog896 <LeoDog896@hotmail.com>
2022-12-01 19:46:15 +0000

    caesar cipher

---
+++ src/routes/caesar/+page.svelte
@@ -0,0 +1,30 @@
+<script lang="ts">
+	const caesar = (text: string, cycle: number): string => {
+		const alphabet = "abcdefghijklmnopqrstuvwxyz".split("");
+
+		return text.split("").map(character => {
+			const lowerChar = character.toLowerCase();
+			if (!alphabet.includes(lowerChar)) return character;
+
+			const isUpperCase = character == character.toUpperCase();
+
+			const cycledCharacter = alphabet[(alphabet.indexOf(lowerChar) + cycle) % alphabet.length];
+
+			console.log(character + cycledCharacter)
+
+			return isUpperCase ? cycledCharacter.toUpperCase() : cycledCharacter;
+		}).join("")
+	}
+
+	let input = ''
+	let amount = 0;
+
+	$: result = caesar(input, amount)
+</script>
+
+<h1>Caesar Cipher</h1>
+
+<input bind:value={input} placeholder="Enter Input">
+<input bind:value={amount} type="number">
+
+<p>{result}</p>
