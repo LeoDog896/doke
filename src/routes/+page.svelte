5f4034d062d47c8bef4a36173d6f245744d96a5b
Tristan F <LeoDog896@hotmail.com>
2022-12-01 16:33:29 +0000

    Begin md5, set up dev container

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,11 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+    import md5 from "md5"
+
+    let input = "";
+</script>
+
+<h1>md5 hasher</h1>
+
+<input bind:value={input}>
+
+<p>{md5(input)}</p>
