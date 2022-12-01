7990f544b70413a37979373896de369c1e219749
Tristan F <LeoDog896@hotmail.com>
2022-12-01 18:45:38 +0000

    random!

---
+++ src/routes/random/+page.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+	function getRandomNumbers(length: number): Uint8Array {
+		if (length <= 0) throw Error("Length must be greater than 0");
+
+		const arr = new Uint8Array(length);
+
+		return crypto.getRandomValues(arr);
+	}
+
+	let numbers = getRandomNumbers(8)
+</script>
+
+<button on:click={() => numbers = getRandomNumbers(8)}>Generate random numbers</button>
+
+<p>{numbers.join(", ")}</p>
