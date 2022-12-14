d9e68bd81f35b2abfa65ed199a15d9ecb50563d1
Tristan F <LeoDog896@hotmail.com>
2022-12-14 13:17:12 +0000

    Vigenere

---
+++ src/routes/vigenere/+page.svelte
@@ -0,0 +1,28 @@
+<script lang="ts" context="module">
+	import { encode, decode } from "@leodog896/vigenere-cipher"
+</script>
+
+<script lang="ts">
+	let toEncode = '';
+	let toEncodeKey = '';
+
+	let toDecode = '';
+	let toDecodeKey = '';
+
+	$: encoded = toEncode.length != 0 && toEncodeKey.length != 0 ? encode(toEncode, toEncodeKey) : '';
+	$: decoded = toDecode.length != 0 && toDecodeKey.length != 0 ? decode(toDecode, toDecodeKey) : '';
+</script>
+
+<h1>Vigenere Cipher Encoder</h1>
+
+<input bind:value={toEncode} placeholder="Enter Input" />
+<input bind:value={toEncodeKey} placeholder="Enter Key" />
+
+<p>{encoded}</p>
+
+<h1>Vigenere Cipher Decoder</h1>
+
+<input bind:value={toDecode} placeholder="Enter Input" />
+<input bind:value={toDecodeKey} placeholder="Enter Key" />
+
+<p>{decoded}</p>
