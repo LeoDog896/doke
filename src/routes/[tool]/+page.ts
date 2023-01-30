1ef38417c2126b1c1bd59f40bc755d928a31fb82
Tristan F <LeoDog896@hotmail.com>
2023-01-30 16:32:00 -0500

    refactor: type data / load

--- src/routes/[tool]/+page.ts
+++ src/routes/[tool]/+page.ts
@@ -1,4 +1,6 @@
-export const load = async ({ params }) => {
+import type { PageLoad } from "./$types"
+
+export const load: PageLoad = async ({ params }) => {
 	return {
 		module: await import(`../../lib/tools/${params.tool}/index.ts`)
 	};
