beae1b8a6fb5cf697b339387b369a858fe0ee53a
Tristan F <LeoDog896@hotmail.com>
2023-01-29 19:56:41 -0500

    feat: atbash

---
+++ src/routes/atbash/+page.svelte
@@ -0,0 +1,28 @@
+<script lang="ts">
+	let input = ''
+
+	function atbash(text: string): string {
+		const alphabet = 'abcdefghijklmnopqrstuvwxyz'.split('')
+		const reversedAlphabet = [...alphabet].reverse()
+
+		return text
+			.split('')
+			.map((character) => {
+				const lowerChar = character.toLowerCase()
+				if (!alphabet.includes(lowerChar)) return character
+
+				const isUpperCase = character == character.toUpperCase()
+
+				const cycledCharacter = reversedAlphabet[alphabet.indexOf(lowerChar)]
+
+				return isUpperCase ? cycledCharacter.toUpperCase() : cycledCharacter
+			})
+			.join('')
+	}
+</script>
+
+<h1>Atbash Cipher</h1>
+
+<input bind:value={input} placeholder="Enter Input" />
+
+<p>{atbash(input)}</p>
