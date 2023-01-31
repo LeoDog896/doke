f5879fa715c2459a4df410a680b554b3d7494242
Tristan F <LeoDog896@hotmail.com>
2023-01-31 15:59:00 +0000

    fix(analysis): pass english options

--- src/lib/tools/analysis/Analysis.svelte
+++ src/lib/tools/analysis/Analysis.svelte
@@ -1,10 +1,13 @@
 <script lang="ts">
+	import { englishOptions } from '$lib/english';
 	import { score } from '$lib/analysis';
 	let toScore = '';
 </script>
 
 <h1>Text Analysis</h1>
 
+<p>Score text based on how likely it is to be English</p>
+
 <input bind:value={toScore} placeholder="Enter Input" />
 
-<p>{score(toScore)}</p>
+<p>{score(toScore, englishOptions)}</p>
