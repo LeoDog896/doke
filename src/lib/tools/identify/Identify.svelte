7386fbd0399d4587648cf5bd75497923e3d7c880
Tristan F <LeoDog896@hotmail.com>
2023-01-31 15:39:24 +0000

    feat: identify

---
+++ src/lib/tools/identify/Identify.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+	const md5 = (text: string) => /^[a-f0-9]{32}$/.test(text);
+
+	let input = ''
+</script>
+
+<h1>Identify</h1>
+
+<p>Programatically determine what type of cipher something is</p>
+
+<input bind:value={input} />
+
+{#if md5(input)}
+	<p>Valid MD5 hash</p>
+{/if}
