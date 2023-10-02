0c6e59180596f22aa653e76ad93e876767a10c1a
Tristan F <26509014+LeoDog896@users.noreply.github.com>
2023-10-02 19:48:15 -0400

    feat: unique tool

---
+++ src/lib/tools/unique/Unique.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+	let toFind = '';
+
+	$: chars = [...new Set(toFind.split(''))].join('');
+</script>
+
+<h1>Unique Character Finder</h1>
+
+<input bind:value={toFind} placeholder="Enter Input" />
+
+<p>Chars: "{chars}"</p>
