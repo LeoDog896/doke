0c76416b417b7e722030aa33556e38c278899686
William Faircloth <willuhmjs@gmail.com>
2023-10-20 09:06:24 -0700

    Add url encode/decode (#6)
    * feat: url decode/encode
    
    * style: categorize enc/dec
    
    ---------
    
    Co-authored-by: Tristan F <LeoDog896@hotmail.com>

---
+++ src/lib/tools/url/Url.svelte
@@ -0,0 +1,26 @@
+<script lang="ts">
+	let toEncrypt = '';
+	$: encryptedResult = globalThis.encodeURI(toEncrypt);
+
+	let toDecrypt = '';
+	let decryptedResult = '';
+	$: {
+		try {
+			decryptedResult = globalThis.decodeURI(toDecrypt);
+		} catch {
+			decryptedResult = 'Result could not be decrypted.';
+		}
+	}
+</script>
+
+<h1>URL Encoder</h1>
+
+<input bind:value={toEncrypt} placeholder="Enter Input" />
+
+<p>{encryptedResult}</p>
+
+<h1>URL Decoder</h1>
+
+<input bind:value={toDecrypt} placeholder="Enter Input" />
+
+<p>{decryptedResult}</p>
